c333b2100a94eb728610b3e983bfb13b3c7c0aed
Manikandan4080 <113871926+Manikandan4080@users.noreply.github.com>
2023-09-28 11:53:53 +0530

    created a submission

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ const sampleObject = { red: "#FF0000", green: "#00FF00", white: "#FFFFFF" };
 function hasKey(key) {
   //   write your code here
 	for(let color in sampleObject){
-		if(key == sampleObject[color]){
+		if(key == sampleObject[color] || key == color){
 			return true
 		}
 	}
@@ -13,4 +13,4 @@ function hasKey(key) {
 
 // Do not change the code below
 const key = prompt("Enter Key.");
-alert(hasKey(key));
+window.alert(hasKey(key));
